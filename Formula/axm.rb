78b38b1581c79a5a7c7d8c1e5f939838ddb4c9d9
Craig Smitham <craig@agentxm.ai>
2026-01-06 13:20:50 -0600

    Update formula URLs to public agentxm/agentxm repo

--- Formula/axm.rb
+++ Formula/axm.rb
@@ -3,13 +3,13 @@
 
 class Axm < Formula
   desc "The official CLI for AgentXM - Agent Experience Management"
-  homepage "https://github.com/agentxm/agentxm-internal"
+  homepage "https://github.com/agentxm/agentxm"
   version "0.1.0-alpha.2"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-darwin-arm64"
+      url "https://github.com/agentxm/agentxm/releases/download/cli-v#{version}/axm-darwin-arm64"
       sha256 "c32d637790565b61cf4c7af5db3e2c7d579566bb9be1fbbd1178116da451fd00"
 
       def install
@@ -18,7 +18,7 @@ class Axm < Formula
     end
 
     on_intel do
-      url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-darwin-x64"
+      url "https://github.com/agentxm/agentxm/releases/download/cli-v#{version}/axm-darwin-x64"
       sha256 "8875302a3b6362c975a95f4a784535b1026f0e150b5ab2d06208efcbcce791f6"
 
       def install
@@ -29,7 +29,7 @@ class Axm < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-linux-arm64"
+      url "https://github.com/agentxm/agentxm/releases/download/cli-v#{version}/axm-linux-arm64"
       sha256 "f04a58e33db28ccec9dba3de24522f04576e59631f6a04fd0f33709a050e78ed"
 
       def install
@@ -38,7 +38,7 @@ class Axm < Formula
     end
 
     on_intel do
-      url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-linux-x64"
+      url "https://github.com/agentxm/agentxm/releases/download/cli-v#{version}/axm-linux-x64"
       sha256 "d094bc59497e34a1384092ed1ca081a67c3799ec03a1ac3d9358265e9e303c9f"
 
       def install
