f1d19b7075915c460110f8aee8f6619e38103265
Craig Smitham <craig@agentxm.ai>
2026-01-06 13:18:13 -0600

    Update axm to 0.1.0-alpha.2

--- Formula/axm.rb
+++ Formula/axm.rb
@@ -4,13 +4,13 @@
 class Axm < Formula
   desc "The official CLI for AgentXM - Agent Experience Management"
   homepage "https://github.com/agentxm/agentxm-internal"
-  version "0.1.0-alpha.0"
+  version "0.1.0-alpha.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-darwin-arm64"
-      sha256 "PLACEHOLDER_DARWIN_ARM64"
+      sha256 "c32d637790565b61cf4c7af5db3e2c7d579566bb9be1fbbd1178116da451fd00"
 
       def install
         bin.install "axm-darwin-arm64" => "axm"
@@ -19,7 +19,7 @@ class Axm < Formula
 
     on_intel do
       url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-darwin-x64"
-      sha256 "PLACEHOLDER_DARWIN_X64"
+      sha256 "8875302a3b6362c975a95f4a784535b1026f0e150b5ab2d06208efcbcce791f6"
 
       def install
         bin.install "axm-darwin-x64" => "axm"
@@ -30,7 +30,7 @@ class Axm < Formula
   on_linux do
     on_arm do
       url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-linux-arm64"
-      sha256 "PLACEHOLDER_LINUX_ARM64"
+      sha256 "f04a58e33db28ccec9dba3de24522f04576e59631f6a04fd0f33709a050e78ed"
 
       def install
         bin.install "axm-linux-arm64" => "axm"
@@ -39,7 +39,7 @@ class Axm < Formula
 
     on_intel do
       url "https://github.com/agentxm/agentxm-internal/releases/download/cli-v#{version}/axm-linux-x64"
-      sha256 "PLACEHOLDER_LINUX_X64"
+      sha256 "d094bc59497e34a1384092ed1ca081a67c3799ec03a1ac3d9358265e9e303c9f"
 
       def install
         bin.install "axm-linux-x64" => "axm"
